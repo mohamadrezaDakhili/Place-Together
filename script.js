dafe95f0181e0f18f81c71a8d9da39f4eb4b248b
mohamadreza dakhili <mohamadrezadakhili53@gmail.com>
2020-03-28 02:27:34 +0430

    change ui button and added book mark in project

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ setTimeout(function() {
 setTimeout(function() {
   $(".cardLoading").fadeOut("fast");
 }, 3000);
+
+//slow scroll when click book marks
+let anchorlinks = document.querySelectorAll('a[href^="#"]');
+
+for (let item of anchorlinks) {
+  // relitere
+  item.addEventListener("click", e => {
+    let hashval = item.getAttribute("href");
+    let target = document.querySelector(hashval);
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+    history.pushState(null, null, hashval);
+    e.preventDefault();
+  });
+}
+
+// fix navbar
+let navbar = document.querySelector("#nav");
+
+window.onscroll = function() {
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 1) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+};
